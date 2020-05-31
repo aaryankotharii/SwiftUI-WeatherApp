548017142e7741715549ba32406a127449458257
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-31 18:07:51 +0530

    CurrentWeatherViewModel added

---
+++ WeatherApp/CurrentWeatherViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  CurrentWeatherViewModel.swift
+//  WeatherApp
+//
+//  Created by Aaryan Kothari on 31/05/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+final class CurrentWeatherViewModel : ObservableObject{
+    
+}
