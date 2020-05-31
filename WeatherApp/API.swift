a04d27947ec8c014658f7f052901fac395f9e95a
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-31 13:17:20 +0530

    API Class added

---
+++ WeatherApp/API.swift
@@ -0,0 +1,17 @@
+//
+//  API.swift
+//  WeatherApp
+//
+//  Created by Aaryan Kothari on 31/05/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import Foundation
+
+class API {
+    
+    
+    class func fetchCurrentWeather(by city : String, onSuccess : (Weather)->()){
+        
+    }
+}
---
+++ WeatherApp/Weather.swift
@@ -0,0 +1,13 @@
+//
+//  Weather.swift
+//  WeatherApp
+//
+//  Created by Aaryan Kothari on 31/05/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import Foundation
+
+struct Weather: Codable {
+    
+}
