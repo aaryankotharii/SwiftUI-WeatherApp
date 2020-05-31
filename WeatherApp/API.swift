5420d30197c6d82dfd8b44b4e474710ed8a7f032
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-31 17:44:30 +0530

    Endpoints minor changes

--- WeatherApp/API.swift
+++ WeatherApp/API.swift
@@ -12,22 +12,24 @@ class API {
     
     enum Endpoints {
         
-        static let dailyBase = "api.openweathermap.org/data/2.5"
+        static let dailyBase = "http://api.openweathermap.org/data/2.5"
         static let weeklyBase = "api.openweathermap.org/data/2.5/forecast"
         static let appid = "9b8cb5423c57be885f14f8033b16ca29"
         
-        case daily(city:String,units:Units)
+        case daily(city:String)
+        
         var stringValue : String {
+            
             switch self {
-                
-            case .daily(city: let city, units: let units):
-                return   Endpoints.dailyBase + "/weather?q=\(city)&APPID=" + Endpoints.appid + "&units=\(units)"
+            case .daily(city: let city):
+                return   Endpoints.dailyBase + "/weather?q=\(city)&APPID=" + Endpoints.appid + "&units=metric"
             }
         }
+        
         var url : URL {
-             return URL(string: self.stringValue)!
-         }
-    }3
+            return URL(string: self.stringValue)!
+        }
+    }
     
     class func fetchCurrentWeather(by city : String, onSuccess : (Weather)->()){
         
