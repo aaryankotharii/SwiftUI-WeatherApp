846a55298c1b3167be8aaa597210c8c8ac421b69
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-31 12:12:17 +0530

    Initial Commit

---
+++ WeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Aaryan Kothari on 31/05/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
