9171856ececae5fbf718059e2e4cdf6fd2298edd
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-31 17:47:38 +0530

    taskForGETRequest added

--- WeatherApp/API.swift
+++ WeatherApp/API.swift
@@ -31,7 +31,33 @@ class API {
         }
     }
     
+    
+    class func taskForGETRequest<ResponseType: Decodable>(url: URL, responseType: ResponseType.Type, completion: @escaping (ResponseType?, Error?) -> Void) -> URLSessionDataTask {
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    completion(nil, error)
+                }
+                return
+            }
+            let decoder = JSONDecoder()
+            do {
+                let responseObject = try decoder.decode(ResponseType.self, from: data)
+                DispatchQueue.main.async {
+                    completion(responseObject, nil)
+                }
+            } catch {
+                completion(nil, error)
+            }
+        }
+        task.resume()
+        
+        return task
+    }
+    
+    
     class func fetchCurrentWeather(by city : String, onSuccess : (Weather)->()){
+        let url = Endpoints.daily(city: city).url
         
     }
 }
