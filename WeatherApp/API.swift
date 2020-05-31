0852fdc5ce926f57f95019feea9c58c781193d45
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-31 22:06:36 +0530

    WeeklyWeather updates

--- WeatherApp/API.swift
+++ WeatherApp/API.swift
@@ -7,22 +7,26 @@
 //
 
 import Foundation
+import MapKit
 
 class API {
     
     enum Endpoints {
         
         static let dailyBase = "http://api.openweathermap.org/data/2.5"
-        static let weeklyBase = "api.openweathermap.org/data/2.5/forecast"
+        static let weeklyBase = "https://api.openweathermap.org/data/2.5/onecall"
         static let appid = "9b8cb5423c57be885f14f8033b16ca29"
         
         case daily(city:String)
+        case weekly(lat:Double,long:Double,dt:Int)
         
         var stringValue : String {
             
             switch self {
             case .daily(city: let city):
                 return   Endpoints.dailyBase + "/weather?q=\(city)&APPID=" + Endpoints.appid + "&units=metric"
+            case .weekly(let lat , let long, let dt):
+                return Endpoints.dailyBase + "/timemachine?lat=\(lat)&lon=\(long)&dt=\(dt)&appid=" + Endpoints.appid + "&units=metric"
             }
         }
         
@@ -56,6 +60,8 @@ class API {
     }
     
     
+    
+    
     class func fetchCurrentWeather(by city : String, completion : @escaping (Weather?)->()){
         let url = Endpoints.daily(city: city).url
         getWeather(url: url, responseType: Weather.self) { (result, error) in
@@ -67,5 +73,50 @@ class API {
             completion(result)
         }
     }
+    
+    class func fetchWeeklyWeather(by city : String, completion : @escaping ([WeeklyWeather?])->()){
+        MapClient.TextToLocation(city) { (location, error) in
+            if let  error = error{
+                print(error.localizedDescription)
+                return
+            }
+            guard let location = location else { return }
+            let lat = location.coordinate.latitude
+            let long = location.coordinate.longitude
+            for dates in Date().weekData{
+                API.getWeather(url: Endpoints.weekly(lat: lat, long: long, dt: dates).url, responseType: WeeklyWeather.self) { (result, error) in
+                    print(result)
+                }
+            }
+        }
+    }
 }
 
+
+
+extension Date {
+    var weekData : [Int]{
+        let days = [-1,-2,-3,-4,-5]
+        let value = days.map {
+            Int(Calendar.current.date(byAdding: .day, value: $0, to: Date())!.timeIntervalSince1970)
+        }
+        
+        return value
+    }
+}
+
+class MapClient {
+    class func TextToLocation(_ address : String, completion: @escaping (CLLocation?, Error?) -> Void){
+        
+        let geoCoder = CLGeocoder()
+        
+        geoCoder.geocodeAddressString(address) { (placemarks, error) in
+            guard let placemarks = placemarks, let location = placemarks.first?.location
+                else {
+                    completion(nil,error)
+                    return
+            }
+            completion(location,nil)
+        }
+    }
+}
--- WeatherApp/CurrentWeatherViewModel.swift
+++ WeatherApp/CurrentWeatherViewModel.swift
@@ -11,7 +11,6 @@ import Combine
 
 final class CurrentWeatherViewModel : ObservableObject{
     @Published var current : Weather?
-    
     init() {
         self.fetch()
     }
