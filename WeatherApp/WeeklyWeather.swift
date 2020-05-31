5812d1f40b01409d2cd196a2e5a8f03dd7ff50b8
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-31 21:05:26 +0530

    WeeklyWeather model added

---
+++ WeatherApp/WeeklyWeather.swift
@@ -0,0 +1,26 @@
+//
+//  WeeklyWeather.swift
+//  WeatherApp
+//
+//  Created by Aaryan Kothari on 31/05/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import Foundation
+
+struct WeeklyWeather : Codable {
+    let  lat , lon : Double
+    let current : Current
+}
+
+struct Current : Codable {
+    let dt, sunrise, sunset, temp, pressure, humidity, clouds, wind_deg, visibility : Int
+    let feels_like, dew_point, uvi, wind_speed: Double
+    let weather : [WeeklyData]
+}
+
+struct WeeklyData : Codable, Identifiable{
+    let id: Int
+    let main, description, icon : String
+}
+
